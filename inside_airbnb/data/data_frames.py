08b9ba08ad683f12aff8f13188eee49c7df47c86
taylor-curran <taylorcurranagtc@gmail.com>
2020-12-16 14:56:49 -0500

    Created Data Frames Module

---
+++ inside_airbnb/data/data_frames.py
@@ -0,0 +1,23 @@
+import pandas as pd
+
+# Ashville
+ashville_lst = pd.read_csv('data/Asheville/listings.csv')
+ashville_rvs = pd.read_csv('data/Asheville/reviews.csv')
+# Austin
+austin_lst = pd.read_csv('data/Austin/listings.csv')
+austin_rvs = pd.read_csv('data/Austin/reviews.csv')
+# Broward County
+broward_lst = pd.read_csv('data/Broward_County/listings.csv')
+broward_rvs = pd.read_csv('data/Broward_County/reviews.csv')
+# Clark County
+clark_lst = pd.read_csv('data/Clark_County/listings.csv')
+clark_rvs = pd.read_csv('data/Clark_County/reviews.csv')
+# Nashville
+nashville_lst = pd.read_csv('data/Nashville/listings.csv')
+nashville_rvs = pd.read_csv('data/Nashville/reviews.csv')
+# Ottawa
+ottawa_lst = pd.read_csv('data/Ottawa/listings.csv')
+ottawa_rvs = pd.read_csv('data/Ottawa/reviews.csv')
+# Salem, OR
+salem_lst = pd.read_csv('data/Salem_OR/listings.csv')
+salem_rvs = pd.read_csv('data/Salem_OR/reviews.csv')
